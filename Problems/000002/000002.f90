8b2aa61e7d80a36b4d72a7a143637e765979a924
bbnia <bbnia@outlook.com>
2024-07-06 14:02:41 +0330

    adding new problem (#2)

---
+++ Problems/000002/000002.f90
@@ -0,0 +1,8 @@
+program SumTwoNumbers
+    implicit none
+    integer :: a, b, sum
+    print *, 'Enter two integers:'
+    read *, a, b
+    sum = a + b
+    print *, 'Sum = ', sum
+  end program SumTwoNumbers  
